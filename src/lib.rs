20700e006fbe1c7ecebc7f061fe81162536458a5
marco <mfernandezpranno@gmail.com>
2017-10-14 16:15:52 +0200

    Small test, and search method.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,10 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     f.read_to_string(&mut contents)?;
 
-    println!("With text : {}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
@@ -31,3 +34,31 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents= "\
+            Rust: safe, fast, productive.
+            Pick three.
+        ";
+
+        assert_eq!(
+            vec!["Rust: safe, fast, productive."], 
+            search(query, contents)
+        );
+    }
+}
